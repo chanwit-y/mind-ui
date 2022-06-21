3e001072624f3a46b51b6a1c4e3ed2c657633b81
chanwit-y <chanwit.yimneam@hotmail.com>
2022-06-21 10:29:09 +0700

    remove react-beautiful-dnd

--- components/toolbox/Toolbox.tsx
+++ components/toolbox/Toolbox.tsx
@@ -1,76 +1,7 @@
 import { Box } from "@mui/material";
 import React, { useState } from "react";
-import {
-  DragDropContext,
-  Draggable,
-  Droppable,
-  DropResult,
-} from "react-beautiful-dnd";
 
-const finalSpaceCharacters = [
-  {
-    id: "gary",
-    name: "Gary Goodspeed",
-  },
-  {
-    id: "cato",
-    name: "Little Cato",
-  },
-  {
-    id: "kvn",
-    name: "KVN",
-  },
-  {
-    id: "mooncake",
-    name: "Mooncake",
-  },
-  {
-    id: "quinn",
-    name: "Quinn Ergon",
-  },
-];
 
 export const Toolbox = () => {
-  const [characters, updateCharacters] = useState(finalSpaceCharacters);
-
-  function handleOnDragEnd(result: DropResult) {
-    if (!result.destination) return;
-
-    const items = Array.from(characters);
-    const [reorderedItem] = items.splice(result.source.index, 1);
-    items.splice(result.destination.index, 0, reorderedItem);
-
-    updateCharacters(items);
-  }
-
-  return (
-    <DragDropContext onDragEnd={handleOnDragEnd}>
-      <Droppable droppableId="characters">
-        {(provided) => (
-          <ul
-            className="characters"
-            {...provided.droppableProps}
-            ref={provided.innerRef}
-          >
-            {characters.map(({ id, name }, index) => {
-              return (
-                <Draggable key={id} draggableId={id} index={index}>
-                  {(provided) => (
-                    <li
-                      ref={provided.innerRef}
-                      {...provided.draggableProps}
-                      {...provided.dragHandleProps}
-                    >
-                      <p>{name}</p>
-                    </li>
-                  )}
-                </Draggable>
-              );
-            })}
-            {provided.placeholder}
-          </ul>
-        )}
-      </Droppable>
-    </DragDropContext>
-  );
+  return <></>
 };
